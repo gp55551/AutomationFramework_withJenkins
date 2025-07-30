610cc49f96406a3347f6ed06cb45a113214f6cf0
gp55551 <patnaikgaurav61@gmail.com>
2025-07-30 12:41:12 +0530

    search product

--- src/main/java/page/HomePage.java
+++ src/main/java/page/HomePage.java
@@ -6,7 +6,11 @@ import org.testng.Assert;
 
 public class HomePage extends BasePage {
 
-    By logo = By.id("nav-logo-sprites");
+    private final By logo = By.id("nav-logo-sprites");
+    private final By searchBox = By.id("twotabsearchtextbox");
+    private final By searchButton = By.id("nav-search-submit-button");
+
+    private final By firstSearchResultName = By.xpath("(//*[text()='Results']/following::a/h2/span)[1]");
 
     public HomePage(WebDriver driver) {
         super(driver);
@@ -16,5 +20,19 @@ public class HomePage extends BasePage {
     {
         waitUntilElementVisible(logo);
         Assert.assertTrue(driver.findElement(logo).isDisplayed());
+    }
+
+    public void searchProduct(String product)
+    {
+        waitUntilElementVisible(searchBox);
+        driver.findElement(searchBox).sendKeys(product);
+        waitUntilElementVisible(searchButton);
+        driver.findElement(searchButton).click();
+    }
+
+    public void verifyFirstLink(String product)
+    {
+        waitUntilElementVisible(firstSearchResultName);
+        Assert.assertTrue(driver.findElement(firstSearchResultName).getText().contains(product));
     }
 }
---
+++ src/test/java/test/SearchProductTest.java
@@ -0,0 +1,23 @@
+package test;
+
+import io.qameta.allure.Description;
+import io.qameta.allure.Severity;
+import io.qameta.allure.Story;
+import org.testng.annotations.Test;
+import page.HomePage;
+
+import static io.qameta.allure.SeverityLevel.CRITICAL;
+import static util.driver.DriverHolder.getDriver;
+
+public class SearchProductTest extends BaseTest {
+
+    @Test(description = "Search Product and Verify the first link")
+    @Severity(CRITICAL)
+    @Description("Search Product and Verify the first link")
+    @Story("As a user I should be able to Search Product and Verify the first link")
+    public void verifyHomePage() {
+        HomePage homePage = new HomePage(getDriver());
+        homePage.searchProduct("iPhone 15");
+        homePage.verifyFirstLink("iPhone 15");
+    }
+}
