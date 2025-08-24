672fc6c17c31551470a17454e85b6cde5ec110fb
gp55551 <patnaikgaurav61@gmail.com>
2025-08-24 09:18:03 +0530

    xml file update

--- src/test/java/test/BaseTest.java
+++ src/test/java/test/BaseTest.java
@@ -3,12 +3,15 @@ package test;
 import io.qameta.allure.Step;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
 import util.driver.DriverFactory;
 
 import static util.PropertyFileReader.getProperty;
 import static util.driver.DriverHolder.getDriver;
 import static util.driver.DriverHolder.setDriver;
+import com.aventstack.chaintest.plugins.ChainTestListener;
 
+@Listeners(ChainTestListener.class)
 public class BaseTest {
 
     @Step("Opening Application")
